34f14c0b636f7a6e57f017d784275ff50dc0c1c3
koshmaymm <michail_koshmay@mail.ru>
2017-09-16 19:08:27 +0300

    change app

--- app.js
+++ app.js
@@ -8,8 +8,42 @@ window.onload = function () {
         .controller('myController', function ($scope, APIservice) {
             APIservice.getData().then(function (response) {
                 $scope.departments = response.data;
-                
-                $scope.dataL = "HI";
+
+                $scope.dataLL;
+
+                $scope.addDepartment = function(val){
+                    
+                    for (var i=0;i<$scope.departments.length;i++){
+                        if ($scope.departments[i]._id == val){
+                            console.log($scope.departments[i]._id);
+                            $scope.dataLL = $scope.departments[i]._id; 
+                        }
+                    }
+                    
+                }
+
+                $scope.editDepartment = function(val){
+                    if($scope.dataLL == undefined) {
+                        return;
+                    }
+                    for (var i=0;i<$scope.departments.length;i++){
+                        if ($scope.departments[i]._id == val){
+                            $scope.departments[i]._id = $scope.dataLL;
+                            $scope.dataLL =  undefined;
+                        }
+                    }
+                }
+
+                $scope.delDepartment = function(val){
+                    if($scope.dataLL !== val) {
+                        return;
+                    }
+                    for (var i=0;i<$scope.departments.length;i++){
+                        if ($scope.departments[i]._id == val){
+                            $scope.departments[i]._id = undefined; 
+                        }
+                    }
+                }
             });
         });
 }
