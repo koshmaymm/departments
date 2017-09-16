43a555bae8fc90a4763e723651d5f83868c560a4
koshmaymm <michail_koshmay@mail.ru>
2017-09-16 23:45:19 +0300

    change app

--- app.js
+++ app.js
@@ -1,49 +1,38 @@
-window.onload = function () {
-    angular.module('myApp', [])
-        .service('APIservice', function APIService($http) {
-            this.getData = function () {
-                return $http.get('dataList.json');
+window.onload = function() {
+    angular.module('myApp', []).service('APIservice', function APIService($http) {
+        this.getData = function() {
+            return $http.get('dataList.json');
+        }
+    }).controller('myController', function($scope, APIservice) {
+        APIservice.getData($scope.addDepartment = function(val) {
+            for (var i = 0; i < $scope.departments.length; i++) {
+                if ($scope.departments[i]._id == val) {
+                    console.log($scope.departments[i]._id);
+                    $scope.dataEmployee = $scope.departments[i]._id;
+                }
             }
-        })
-        .controller('myController', function ($scope, APIservice) {
-            APIservice.getData().then(function (response) {
-                $scope.departments = response.data;
-
-                $scope.dataLL;
-
-                $scope.addDepartment = function(val){
-                    
-                    for (var i=0;i<$scope.departments.length;i++){
-                        if ($scope.departments[i]._id == val){
-                            console.log($scope.departments[i]._id);
-                            $scope.dataLL = $scope.departments[i]._id; 
-                        }
-                    }
-                    
+        }, $scope.editDepartment = function(val) {
+            if ($scope.dataEmployee == undefined) {
+                return;
+            }
+            for (var i = 0; i < $scope.departments.length; i++) {
+                if ($scope.departments[i]._id == val) {
+                    $scope.departments[i]._id = $scope.dataEmployee;
+                    $scope.dataEmployee = undefined;
                 }
-
-                $scope.editDepartment = function(val){
-                    if($scope.dataLL == undefined) {
-                        return;
-                    }
-                    for (var i=0;i<$scope.departments.length;i++){
-                        if ($scope.departments[i]._id == val){
-                            $scope.departments[i]._id = $scope.dataLL;
-                            $scope.dataLL =  undefined;
-                        }
-                    }
+            }
+        }, $scope.delDepartment = function(val) {
+            if ($scope.dataEmployee !== val) {
+                return;
+            }
+            for (var i = 0; i < $scope.departments.length; i++) {
+                if ($scope.departments[i]._id == val) {
+                    $scope.departments[i]._id = undefined;
                 }
-
-                $scope.delDepartment = function(val){
-                    if($scope.dataLL !== val) {
-                        return;
-                    }
-                    for (var i=0;i<$scope.departments.length;i++){
-                        if ($scope.departments[i]._id == val){
-                            $scope.departments[i]._id = undefined; 
-                        }
-                    }
-                }
-            });
+            }
+        }).then(function(response) {
+            $scope.departments = response.data;
+            $scope.dataEmployee;
         });
+    });
 }
