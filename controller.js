25342c6f4e77fc9d6a5f2b70419b48be29f34126
koshmaymm <michail_koshmay@mail.ru>
2017-09-17 13:19:36 +0300

    change controller

--- controller.js
+++ controller.js
@@ -1,32 +1,28 @@
     angular.module('myApp')
     .controller('myController', function($scope, APIservice) {
         APIservice.getData($scope.addDepartment = function(val) {
-            for (var i = 0; i < $scope.departments.length; i++) {
-                if ($scope.departments[i]._id == val) {
-                    console.log($scope.departments[i]._id);
-                    $scope.dataEmployee = $scope.departments[i]._id;
+
+            angular.forEach($scope.departments, function(value, key) {
+                if ($scope.departments[key]._id == val) {
+                    $scope.dataEmployee = $scope.departments[key]._id; 
                 }
-            }
+            });
+
         }, $scope.editDepartment = function(val) {
             if ($scope.dataEmployee == undefined) {
                 return;
             }
-            for (var i = 0; i < $scope.departments.length; i++) {
-                if ($scope.departments[i]._id == val) {
-                    $scope.departments[i]._id = $scope.dataEmployee;
-                    $scope.dataEmployee = undefined;
+            angular.forEach($scope.departments, function(value, key) {
+                if ($scope.departments[key]._id == val) {
+                    $scope.departments[key]._id = $scope.dataEmployee;
+                    $scope.dataEmployee = undefined; 
                 }
-            }
+            });
+
         }, $scope.delDepartment = function(val) {
             if ($scope.dataEmployee !== val) {
                 return;
             }
-            /*for (var i = 0; i < $scope.departments.length; i++) {
-                if ($scope.departments[i]._id == val) {
-                    $scope.departments[i]._id = undefined;
-                    $scope.dataEmployee = undefined;
-                }
-            }*/
 
             angular.forEach($scope.departments, function(value, key) {
                 if ($scope.departments[key]._id == val) {
