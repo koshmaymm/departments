9ca69606952d6bf29a2565f6b372ff65ae5f0592
koshmaymm <michail_koshmay@mail.ru>
2017-09-17 13:30:07 +0300

    change controller

--- controller.js
+++ controller.js
@@ -1,13 +1,10 @@
-    angular.module('myApp')
-    .controller('myController', function($scope, APIservice) {
+    angular.module('myApp').controller('myController', function($scope, APIservice) {
         APIservice.getData($scope.addDepartment = function(val) {
-
             angular.forEach($scope.departments, function(value, key) {
                 if ($scope.departments[key]._id == val) {
-                    $scope.dataEmployee = $scope.departments[key]._id; 
+                    $scope.dataEmployee = $scope.departments[key]._id;
                 }
             });
-
         }, $scope.editDepartment = function(val) {
             if ($scope.dataEmployee == undefined) {
                 return;
@@ -15,22 +12,19 @@
             angular.forEach($scope.departments, function(value, key) {
                 if ($scope.departments[key]._id == val) {
                     $scope.departments[key]._id = $scope.dataEmployee;
-                    $scope.dataEmployee = undefined; 
+                    $scope.dataEmployee = undefined;
                 }
             });
-
         }, $scope.delDepartment = function(val) {
             if ($scope.dataEmployee !== val) {
                 return;
             }
-
             angular.forEach($scope.departments, function(value, key) {
                 if ($scope.departments[key]._id == val) {
                     $scope.departments[key]._id = undefined;
                     $scope.dataEmployee = undefined;
                 }
             });
-
         }).then(function(response) {
             $scope.departments = response.data;
             $scope.dataEmployee;
