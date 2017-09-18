c12f80862c79e61ffefa3ccce8bed28ec3207ee9
koshmaymm <michail_koshmay@mail.ru>
2017-09-18 17:21:43 +0300

    change controller

--- controller.js
+++ controller.js
@@ -1,59 +1,55 @@
     angular.module('myApp').controller('myController', function($scope, APIservice) {
+        $scope.dataEmployee;
+        $scope.departmentForEditOrDelete;
+
         APIservice.getData().then(function(response) {
             $scope.departments = response.data;
-            $scope.departmentName;
-            $scope.departmentForEditOrDelete;
-
-            $scope.addDepartment = function(val) {
-                /*angular.forEach($scope.departments, function(value, key) {
-                    if ($scope.departments[key]._id == val) {
-                        $scope.dataEmployee = $scope.departments[key]._id;
-                    }
-                });*/
-                $scope.departments.push({
-                    "_id": val,
-                    "index": ($scope.departments.length + 1),
-                    "names": []
-                });
-               // $scope.clearData();
-            };
-            
-            $scope.editDepartment = function(val) {
-                if (($scope.departmentName == undefined) || (!angular.isNumber($scope.departmentForEditOrDelete))) {
-                    return;
-                }
-               /* angular.forEach($scope.departments, function(value, key) {
-                    if ($scope.departments[key]._id == val) {
-                        $scope.departments[key]._id = $scope.dataEmployee;
-                        $scope.dataEmployee = undefined;
-                    }
-                });*/
-                //console.log($scope.dataEmployee);
-                $scope.departments[$scope.departmentForEditOrDelete]._id = $scope.departmentName;
-                //$scope.clearData();
-            };
-
-            $scope.delDepartment = function(val) {
-                /*angular.forEach($scope.departments, function(value, key) {
-                    if ($scope.departments[key]._id == val) {
-                        $scope.departments[key]._id = undefined;
-                        $scope.dataEmployee = undefined;
-                    }
-                });*/
-                    console.log($scope.departmentForEditOrDelete);
-                    $scope.departments[$scope.departmentForEditOrDelete]._id == undefined;
-                    //$scope.clearData();
-                
-            }
-
-            $scope.getIndex = function(val){
-                $scope.departmentForEditOrDelete = val;
-                //console.log($scope.departmentForEditOrDelete); 
-            }
-            $scope.clearData = function(){
-                $scope.departmentName = undefined;
-                $scope.departmentForEditOrDelete = undefined;
-                //document.querySelectorAll("departmentGroup").checked = false;
-            }
         });
+
+        $scope.addDepartment = function(val) {
+            $scope.departments.push({
+                "_id": val,
+                "index": ($scope.departments.length + 1),
+                "names": []
+            });
+            $scope.clearData();
+        };
+
+        $scope.editDepartment = function(val) {
+            if (!$scope.dataEmployee) {
+                return;
+            }
+
+            angular.forEach($scope.departments, function(value, key) {
+                if ($scope.departments[key]._id === $scope.departmentForEditOrDelete) {
+                    $scope.departments[key]._id = $scope.dataEmployee;
+                    $scope.clearData();
+                }
+
+            });
+        };
+
+        $scope.delDepartment = function(val) {
+            if (!$scope.departmentForEditOrDelete) {
+                return;
+            }
+
+            angular.forEach($scope.departments, function(value, key) {
+                if ($scope.departments[key]._id === $scope.departmentForEditOrDelete) {
+                    $scope.departments[key]._id = undefined;
+                }
+
+            });
+            $scope.clearData();
+        }
+
+        $scope.getIndex = function(val) {
+            $scope.departmentForEditOrDelete = val;
+        }
+
+        $scope.clearData = function() {
+            $scope.dataEmployee = undefined;
+            $scope.departmentForEditOrDelete;
+            document.getElementById("myForm").reset();
+        }
     });
