b694dfe8682852d10835645c73325b83025a21f7
koshmaymm <michail_koshmay@mail.ru>
2017-09-18 09:27:07 +0300

    change controller

--- controller.js
+++ controller.js
@@ -1,32 +1,39 @@
     angular.module('myApp').controller('myController', function($scope, APIservice) {
-        APIservice.getData($scope.addDepartment = function(val) {
-            angular.forEach($scope.departments, function(value, key) {
-                if ($scope.departments[key]._id == val) {
-                    $scope.dataEmployee = $scope.departments[key]._id;
-                }
-            });
-        }, $scope.editDepartment = function(val) {
-            if ($scope.dataEmployee == undefined) {
-                return;
-            }
-            angular.forEach($scope.departments, function(value, key) {
-                if ($scope.departments[key]._id == val) {
-                    $scope.departments[key]._id = $scope.dataEmployee;
-                    $scope.dataEmployee = undefined;
-                }
-            });
-        }, $scope.delDepartment = function(val) {
-            if ($scope.dataEmployee !== val) {
-                return;
-            }
-            angular.forEach($scope.departments, function(value, key) {
-                if ($scope.departments[key]._id == val) {
-                    $scope.departments[key]._id = undefined;
-                    $scope.dataEmployee = undefined;
-                }
-            });
-        }).then(function(response) {
+        APIservice.getData().then(function(response) {
             $scope.departments = response.data;
             $scope.dataEmployee;
+
+            $scope.addDepartment = function(val) {
+                angular.forEach($scope.departments, function(value, key) {
+                    if ($scope.departments[key]._id == val) {
+                        $scope.dataEmployee = $scope.departments[key]._id;
+                    }
+                });
+            };
+            
+            $scope.editDepartment = function(val) {
+                if ($scope.dataEmployee == undefined) {
+                    return;
+                }
+                angular.forEach($scope.departments, function(value, key) {
+                    if ($scope.departments[key]._id == val) {
+                        $scope.departments[key]._id = $scope.dataEmployee;
+                        $scope.dataEmployee = undefined;
+                    }
+                });
+            };
+
+            $scope.delDepartment = function(val) {
+                if ($scope.dataEmployee !== val) {
+                    return;
+                }
+                angular.forEach($scope.departments, function(value, key) {
+                    if ($scope.departments[key]._id == val) {
+                        $scope.departments[key]._id = undefined;
+                        $scope.dataEmployee = undefined;
+                    }
+                });
+            }
+
         });
     });
