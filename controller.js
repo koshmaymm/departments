2fe48eca271520e6cfcf5fd32251d55bf99ade6c
koshmaymm <michail_koshmay@mail.ru>
2017-09-18 15:07:21 +0300

    change controller

--- controller.js
+++ controller.js
@@ -1,7 +1,8 @@
     angular.module('myApp').controller('myController', function($scope, APIservice) {
         APIservice.getData().then(function(response) {
             $scope.departments = response.data;
-            $scope.dataEmployee;
+            $scope.departmentName;
+            $scope.departmentForEditOrDelete;
 
             $scope.addDepartment = function(val) {
                 /*angular.forEach($scope.departments, function(value, key) {
@@ -14,29 +15,45 @@
                     "index": ($scope.departments.length + 1),
                     "names": []
                 });
-                $scope.dataEmployee = undefined;
+               // $scope.clearData();
             };
             
             $scope.editDepartment = function(val) {
-                if ($scope.dataEmployee == undefined) {
+                if (($scope.departmentName == undefined) || (!angular.isNumber($scope.departmentForEditOrDelete))) {
                     return;
                 }
-                angular.forEach($scope.departments, function(value, key) {
+               /* angular.forEach($scope.departments, function(value, key) {
                     if ($scope.departments[key]._id == val) {
                         $scope.departments[key]._id = $scope.dataEmployee;
                         $scope.dataEmployee = undefined;
                     }
-                });
+                });*/
+                //console.log($scope.dataEmployee);
+                $scope.departments[$scope.departmentForEditOrDelete]._id = $scope.departmentName;
+                //$scope.clearData();
             };
 
             $scope.delDepartment = function(val) {
-                angular.forEach($scope.departments, function(value, key) {
+                /*angular.forEach($scope.departments, function(value, key) {
                     if ($scope.departments[key]._id == val) {
                         $scope.departments[key]._id = undefined;
                         $scope.dataEmployee = undefined;
                     }
-                });
+                });*/
+                    console.log($scope.departmentForEditOrDelete);
+                    $scope.departments[$scope.departmentForEditOrDelete]._id == undefined;
+                    //$scope.clearData();
+                
             }
 
+            $scope.getIndex = function(val){
+                $scope.departmentForEditOrDelete = val;
+                //console.log($scope.departmentForEditOrDelete); 
+            }
+            $scope.clearData = function(){
+                $scope.departmentName = undefined;
+                $scope.departmentForEditOrDelete = undefined;
+                //document.querySelectorAll("departmentGroup").checked = false;
+            }
         });
     });
